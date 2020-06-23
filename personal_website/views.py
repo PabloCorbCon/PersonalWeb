3430b592dfc79ca0454384b15aee549fc4c76cb7
Pablo Corbalán De Concepción <63709477+PabloCorbCon@users.noreply.github.com>
2020-06-23 10:52:04 +0200

    index template added

---
+++ personal_website/views.py
@@ -0,0 +1,24 @@
+from django.http import HttpResponse
+from django.template import Template, Context
+
+def main_page(request):
+
+	'''This is the main view for the personal web. Its
+	asociated to the template located in structure/index.html
+
+	The htm document is saved in a variable called 'doc'
+	and after that it's charged in a Template object called ptx. The context
+	is created in a Context object and html is the final view, the ptx object
+	after calling the .render() method with ctx as argument.'''
+
+	doc = open('structure/index.html')
+
+	ptx = Template(doc.read())
+
+	doc.close()
+
+	ctx = Context()
+
+	html = ptx.render(ctx)
+	
+	return HttpResponse(html)
