8dea52bcee1760e848e548b10f8fa1b29fc10747
Pablo Corbalán De Concepción <63709477+PabloCorbCon@users.noreply.github.com>
2020-08-05 00:32:47 +0200

    jirja2 bugs solved

--- app.py
+++ app.py
@@ -3,10 +3,25 @@ from flask import Flask, render_template, url_for
 
 app = Flask(__name__)
 
+posts = [
+	{
+		'title':'Stork',
+		'short_description':'Online local files manager'
+	},
+	{
+		'title':'project 2',
+		'short_description':'project 2 description',
+	},
+	{
+		'title':'project 3',
+		'short_description':'project 3 description',
+	},
+]
+
 @app.route('/')
 def main_page():
 
-	return render_template('main.html')
+	return render_template('main.html', posts=posts)
 
 if __name__ == '__main__':
 	
