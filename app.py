9ef4a340189dcd3fcac6030d4deb65b857307ab2
Pablo Corbalán De Concepción <63709477+PabloCorbCon@users.noreply.github.com>
2020-08-31 15:49:15 +0200

    style reseted

--- app.py
+++ app.py
@@ -3,25 +3,10 @@ from flask import Flask, render_template, url_for
 
 app = Flask(__name__)
 
-projects = [
-	{
-		'title':'Stork',
-		'short_description':'Online local files manager'
-	},
-	{
-		'title':'project 2',
-		'short_description':'project 2 description',
-	},
-	{
-		'title':'project 3',
-		'short_description':'project 3 description',
-	},
-]
-
 @app.route('/')
 def home():
 
-	return render_template('main.html', projects=projects)
+	return render_template('main.html')
 
 if __name__ == '__main__':
 	
