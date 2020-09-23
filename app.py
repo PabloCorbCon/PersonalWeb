71aa7e195e9543265c6e19349166f7141a7a05f1
PabloCorbCon <pablocorbcon@gmail.com>
2020-09-23 14:54:54 +0200

    first pages createdd

--- app.py
+++ app.py
@@ -5,7 +5,10 @@ app = Flask(__name__)
 
 @app.route('/')
 def home():
-	return render_template('main.html')
+	return render_template('home.html')
+@app.route('/contact')
+def contact():
+	return render_template('contact.html', title='contact', cards=[2, 3, 4, 5, 6])
 
 if __name__ == '__main__':
 	app.run(debug =True, port=5000)
