72628627efa2a1ef0a0be6a780097f3e27472780
PabloCorbCon <pablocorbcon@gmail.com>
2020-10-04 16:29:45 +0200

    error 404 implemented in the page

--- app.py
+++ app.py
@@ -12,14 +12,13 @@ def contact():
 	return render_template('contact.html', title='contact', cards=[2, 3, 4, 5, 6])
 
 @app.route('/error/404')
-@app.errorhandler(404):
-def error404():
+@app.errorhandler(404)
+def error404(err=None):
 	e = {
-		'title':'Page not found',
 		'number':'404',
-		'description':'This page does not exist or it is a private page.<br>Check that you typed the link in the correct way.' 
+		'description':'We could not find the page you are looking for, perhaps you have misspelled the link or the page is private.' 
 	}
-	return render_template('error.html', title='404', error=e)
+	return render_template('error.html', title='404', error=e), 404
 
 if __name__ == '__main__':
 	app.run(debug =True, port=5000)
