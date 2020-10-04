40fc622b455a198585d554e740f9ae13b28e89af
PabloCorbCon <pablocorbcon@gmail.com>
2020-10-04 15:59:16 +0200

    404 error route created

--- app.py
+++ app.py
@@ -11,5 +11,15 @@ def home():
 def contact():
 	return render_template('contact.html', title='contact', cards=[2, 3, 4, 5, 6])
 
+@app.route('/error/404')
+@app.errorhandler(404):
+def error404():
+	e = {
+		'title':'Page not found',
+		'number':'404',
+		'description':'This page does not exist or it is a private page.<br>Check that you typed the link in the correct way.' 
+	}
+	return render_template('error.html', title='404', error=e)
+
 if __name__ == '__main__':
 	app.run(debug =True, port=5000)
