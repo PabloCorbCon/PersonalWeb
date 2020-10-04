43c5a299dd8849bcfe93d55c413ff32f137c4026
PabloCorbCon <pablocorbcon@gmail.com>
2020-10-04 16:35:42 +0200

    routes in app.py documented

--- app.py
+++ app.py
@@ -5,20 +5,42 @@ app = Flask(__name__)
 
 @app.route('/')
 def home():
+	"""
+	This is the main function of the flask page,
+	it uses the render_template() function to return the
+	home.html HTML template.
+
+	:parameters None:
+	"""
 	return render_template('home.html')
 	
 @app.route('/contact')
 def contact():
+	"""
+	This is the contact page of the app, it uses the
+	render_template() function to return the contacts.html
+	HTML template.
+	"""
 	return render_template('contact.html', title='contact', cards=[2, 3, 4, 5, 6])
 
 @app.route('/error/404')
 @app.errorhandler(404)
 def error404(err=None):
+	"""
+	This handler error function is used to
+	create custom 404 error templates.
+	It uses the @app.errorhandler() decorator
+	to handle the error itself.
+	"""
 	e = {
 		'number':'404',
 		'description':'We could not find the page you are looking for, perhaps you have misspelled the link or the page is private.' 
 	}
 	return render_template('error.html', title='404', error=e), 404
+
+
+
+
 
 if __name__ == '__main__':
 	app.run(debug =True, port=5000)
