60154196e5e8fb7d08558b5263f44e816351516e
Pablo Corbalán <pablocorbcon@gmail.com>
2020-10-11 17:04:15 +0200

    projects deleted

--- app.py
+++ app.py
@@ -12,26 +12,6 @@ def home():
 
 	:parameters None:
 	"""
-	projects = [
-		{
-			'image': 'https://raw.githubusercontent.com/PabloCorbCon/prodify/main/src/static/images/logo.png',
-			'name': 'prodify',
-			'description':' Prodify is a web page created by students for students for the October 2020 Tech with Tim code jam.',
-			'code':' https://github.com/PabloCorbCon/prodify'
-		},
-		{
-			'image': 'https://raw.githubusercontent.com/pythonmurcia/gitcollab/master/branding/logo.png',
-			'name': 'gitcollab',
-			'description':' GitCollab is a web page I presented from Python Murcia to the HacktoberfestES 2020.',
-			'code':' https://github.com/pythonmurcia/gitcollab'
-		},
-		{
-			'image': 'https://raw.githubusercontent.com/PabloCorbCon/Taur/master/branding/grey-logo.png',
-			'name': 'Taur',
-			'description':'Taur is a multifunctional discord bot I created using the discord.py library. I am still working on it.',
-			'code':' https://github.com/PabloCorbCon/prodify'
-		}
-	]
 	icons = [
 		{
 			'url':'https://twitter.com/pablocorbcon',
@@ -54,7 +34,7 @@ def home():
 			'route':'telegram'
 		}
 	]
-	return render_template('home.html', projects=projects, icons=icons)
+	return render_template('home.html', icons=icons)
 
 @app.route('/error')
 def error():
